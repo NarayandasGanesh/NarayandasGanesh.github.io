849afd11eef8a162f6b691047a1534bfa8124931
unknown <NarayandasGanesh>
2022-11-28 01:36:54 +0530

    Portfolio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ColorModeScript } from '@chakra-ui/react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ColorModeScript />
     <App />
   </React.StrictMode>
 );
